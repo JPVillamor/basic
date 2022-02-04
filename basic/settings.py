79006a2c0db988178622207cca494f724c083c6d
JP Villamor <jpvillam@mailfence.com>
2022-02-04 18:04:23 -0500

    added allowed hosts

--- basic/settings.py
+++ basic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b*q&6#%x^9#ev8bfkw3!t!pi!sguh8cxot2cll(+)v6zp%bq8q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
